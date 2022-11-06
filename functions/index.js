34d29e9148ab21ddc0de6581101148c6abc3df20
Matthew McDevitt <mmcdevitt1997@gmail.com>
2022-11-06 08:59:30 -0600

    creating new users

--- functions/index.js
+++ functions/index.js
@@ -18,9 +18,10 @@ const config = {
 const firebase = require("firebase");
 firebase.initializeApp(config);
 
+const db = admin.firestore();
+
 app.get("/screams", (req, res) =>{
-  admin
-      .firestore()
+  db
       .collection("screams")
       .orderBy("createdAt", "desc")
       .get()
@@ -46,8 +47,7 @@ app.post("/scream", (req, res) => {
     userhandle: req.body.userHandle,
     createdAt: new Date().toISOString(),
   };
-  admin
-      .firestore()
+  db
       .collection("screams")
       .add(newScream)
       .then((doc) => {
@@ -70,8 +70,29 @@ app.post("/signup", (req, res) => {
     handle: req.body.handle,
   };
   //  TODO: validate data
-  // eslint-disable-next-line max-len
-  firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+  db.doc(`/users/${newUser.handle}`).get()
+      .then((doc) => {
+        if (doc.exists) {
+          return res.status(400).json({handle: "this handle is already taken"});
+        } else {
+          return firebase
+              .auth()
+              .createUserWithEmailAndPassword(newUser.email, newUser.password);
+        }
+      })
+      .then((data) => {
+        return data.user.getIdToken();
+      })
+      .then((token) => {
+        return res.status(201).json({token});
+      })
+      .catch((err) => {
+        console.error(err);
+        return res.status(500).json({error: err});
+      });
+
+  firebase.auth()
+      .createUserWithEmailAndPassword(newUser.email, newUser.password)
       .then( (data) => {
         return res.status(201).json({
           message: `user ${ data.user.uid } signed up succussfully`,
@@ -82,3 +103,4 @@ app.post("/signup", (req, res) => {
         console.error(err);
       });
 });
+
